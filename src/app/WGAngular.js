b6f2c3748da9c233699ae1cbacebf9ae3d513f2f
Denis Lutcevich <DenisLutcevich@corp.fairisaac.com>
2016-07-13 16:47:14 +0300

    Add modalController

--- src/app/WGAngular.js
+++ src/app/WGAngular.js
@@ -1,8 +1,10 @@
 /**
  * Created by DenisLutcevich on 01/07/2016.
  */
-var app = angular.module('WGAngular', []),
-    data = {
+var app = angular.module('WGAngular', []);
+
+app.controller('EaselController', ['$scope', function ($scope) {
+    $scope.model = {
         info: 'test',
         selectedElements: [{
             id: 1,
@@ -16,27 +18,34 @@ var app = angular.module('WGAngular', []),
         }]
     };
 
-app.controller('EaselController', function () {
     this.initModel = function () {
-        this.model = data;
-
         this.updateModelInfo();
     };
 
     this.updateModelInfo = function () {
-        var quantity = this.model.selectedElements.length;
+        var quantity = $scope.model.selectedElements.length;
 
-        this.model.info = quantity > 0 ? quantity + ' elements' : quantity + ' element';
+        $scope.model.info = quantity > 0 ? quantity + ' elements' : quantity + ' element';
     };
 
 
     this.deletedSelected = function (id) {
-        var index = _.findIndex(this.model.selectedElements, {'id': id});
+        var index = _.findIndex($scope.model.selectedElements, {'id': id});
 
-        this.model.selectedElements.splice(index, 1);
+        $scope.selectedElements.splice(index, 1);
 
         this.updateModelInfo();
     };
 
+    this.changeSelection = function () {
+
+    };
+
     this.initModel();
-});
+}]);
+
+app.controller('ModalController', ['$scope', function ($scope) {
+
+}]);
+
+app.service();
