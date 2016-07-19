0e225ce41501d9d7c787ef82f2e07e9284ce2e16
Denis Lutcevich <DenisLutcevich@corp.fairisaac.com>
2016-07-19 17:49:03 +0300

    add WGAngular controller

--- src/app/WGAngular.js
+++ src/app/WGAngular.js
@@ -5,47 +5,16 @@
 
 var app = angular.module('WGAngular', ['ngResource']);
 
-app.controller('EaselController', ['$scope', function ($scope) {
-    $scope.model = {
-        info: 'test',
-        selectedElements: [{
-            id: 1,
-            name: 'test'
-        }, {
-            id: 2,
-            name: 'test1'
-        }, {
-            id: 3,
-            name: 'test2'
-        }]
-    };
+app.controller('WGController', ['$scope', 'modelGetter', 'selectedItemsModelGetter', 'modelUpdater', function ($scope, dataGetter, selectedItemsModelGetter, modelUpdater) {
+    dataGetter.query().$promise.then(function (model) {
+        $scope.model = model;
+        $scope.selectedItemsModel = $scope.selectedItemsModel || selectedItemsModelGetter.query($scope.model);
 
-    this.initModel = function () {
-        this.updateModelInfo();
-    };
-
-    this.updateModelInfo = function () {
-        var quantity = $scope.model.selectedElements.length;
-
-        $scope.model.info = quantity > 0 ? quantity + ' elements' : quantity + ' element';
-    };
-
-
-    this.deletedSelected = function (id) {
-        var index = _.findIndex($scope.model.selectedElements, {'id': id});
-
-        $scope.selectedElements.splice(index, 1);
-
-        this.updateModelInfo();
-    };
-
-    this.changeSelection = function () {
-    };
-
-    this.initModel();
+        modelUpdater.update($scope.selectedItemsModel);
+    });
 }]);
 
-app.factory('dataGetter', ['$resource', function ($resource) {
+app.factory('modelGetter', ['$resource', function ($resource) {
     return $resource('../../public/data/:id.json', {}, {
         query: {
             method: 'GET',
@@ -55,6 +24,73 @@ app.factory('dataGetter', ['$resource', function ($resource) {
     });
 }]);
 
-app.controller('ModalController', ['$scope', 'dataGetter', function ($scope, dataGetter) {
-    this.model = dataGetter.query();
+app.service('selectedItemsModelGetter', [function () {
+    this.query = function (model) {
+        var _model = [], i, ln;
+
+        for (i = 0, ln = 3; i < ln; i++) {
+            _model.push(model[i]);
+        }
+
+        return _model;
+    }
+}]);
+
+app.service('modelUpdater', [function () {
+    var selectedInfo = {};
+
+    selectedInfo.text = '';
+
+    this.getSelectedInfo = function () {
+      return selectedInfo;
+    };
+
+    this.update = function (model) {
+        var quantity = model.length;
+
+        selectedInfo.text = quantity > 0 ? quantity + ' elements' : quantity + ' element';
+    };
+
+    return {
+        getSelectedInfo: this.getSelectedInfo,
+        update: this.update
+    };
+}]);
+
+app.controller('EaselController', ['$scope', 'modelUpdater', function ($scope, modelUpdater) {
+    var self = this;
+
+    self.selectedInfo.text = modelUpdater.getSelectedInfo();
+
+    $scope.$watch('selectedInfo', function (newValue, oldValue) {
+        debugger;
+        if (newValue !== oldValue) {
+            self.selectedInfo = modelUpdater.getSelectedInfo();
+        }
+    });
+
+    this.deletedSelected = function (id) {
+        var index = _.findIndex($scope.model.selectedElements, {'id': id});
+
+        $scope.model.selectedElements.splice(index, 1);
+
+        this.updateModelInfo();
+    };
+
+    this.changeSelection = function () {
+    };
+}]);
+
+app.controller('ModalController', ['$scope', 'modelGetter', function ($scope, dataGetter) {
+}]);
+
+app.directive('selectedElement', [function () {
+    function link(scope, element, attr) {
+    }
+
+    return {
+        restrict: 'E',
+        templateUrl: '../../public/templates/selectedElement.html',
+        link: link
+    };
 }]);
